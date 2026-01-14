7361b247252ea2af7d8782f6463e450722209787
getwinharris <getwinharris@gmail.com>
2026-01-14 16:37:32 +0000

    Update NNL Corporate and Investor features

---
+++ catalog/controller/information/directors.php
@@ -0,0 +1,31 @@
+<?php
+class ControllerInformationDirectors extends Controller {
+	public function index() {
+		$this->load->language('information/information');
+
+		$this->document->setTitle($this->language->get('text_directors'));
+
+		$data['breadcrumbs'] = [];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		];
+
+		$data['breadcrumbs'][] = [
+			'text' => $this->language->get('text_directors'),
+			'href' => $this->url->link('information/directors')
+		];
+
+		$data['continue'] = $this->url->link('common/home');
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('information/directors', $data));
+	}
+}
